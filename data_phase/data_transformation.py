ed258b0b03f4ad5379658265a9b4b42e898840bc
vjvivekvj <vivekj.joshi9@gmail.com>
2021-06-06 23:34:12 +0530

    added column for percentage change and past trend

--- data_phase/data_transformation.py
+++ data_phase/data_transformation.py
@@ -1,16 +1,28 @@
 import pandas as pd
-csv_input = pd.read_csv('cleaned_dataset.csv')
+csv_input = pd.read_csv('results/cleaned_dataset.csv')
 
 price = 'BTC_PRICE'
 changes = [0]
+pchange = [0]
+trend = [0]
 for i in range(1,len(csv_input)):
     if csv_input[price][i] - csv_input[price][i-1] > 0:
         changes.append(1)
+        trend.append(trend[-1]+1)
+        
     elif csv_input[price][i] - csv_input[price][i-1] < 0:
         changes.append(-1)
+        trend.append(trend[-1]-1)
     else:
         changes.append(0)
+        trend.append(trend[-1])
+    
+    _pchange = ((csv_input[price][i] - csv_input[price][i-1])/csv_input[price][i-1]) * 100
+    pchange.append("{:.2f}".format(_pchange))
+    
     
 csv_input['CHANGE'] = changes
+csv_input['PCHANGE'] = pchange
+csv_input['TREND'] = trend
     
-csv_input.to_csv('transformed.csv', index=False)
+csv_input.to_csv('results/transformed_dataset.csv', index=False)
