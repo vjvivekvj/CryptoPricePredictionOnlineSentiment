6c3ebe21e4b03e516dc61cb5de623605cfb3a0c7
vjvivekvj <vivekj.joshi9@gmail.com>
2021-06-08 15:38:08 +0530

    added classes for decision tree

--- Decision_Tree/decision_tree.py
+++ Decision_Tree/decision_tree.py
@@ -1,5 +1,22 @@
-from discretization.discretization import *
+import pandas as pd
+from sklearn import tree
 
-def build_model():
-    model = equidistant_bining()
-    return model
+class DTree:
+        
+    def __init__(self):
+        self.model = tree.DecisionTreeClassifier()
+        self.isFit = False
+    def generate_model(self, input, target):
+        self.model.fit(input, target)
+        self.isFit = True
+    
+    def model_score(self,inputs,target):
+        if self.isFit:
+            return self.model.score(inputs,target)
+        else:
+            print("Fit the model first")
+    
+    def create_ruleset(self):
+        pass
+    
+    
