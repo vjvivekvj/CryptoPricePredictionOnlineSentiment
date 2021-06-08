413ac1d0676f5b6a0c9309c0c8efd303461771f2
vjvivekvj <vivekj.joshi9@gmail.com>
2021-06-08 15:38:54 +0530

    added classes for discretization

--- discretization/discretization.py
+++ discretization/discretization.py
@@ -6,8 +6,9 @@ class Discretize:
     csv_input = "data_phase/results/transformed_dataset.csv"
     df = pd.read_csv(csv_input) 
     
-    def __init__(self):
-        self.input = Discretize.df.drop('CHANGE',axis='columns')    
+    def __init__(self,input):
+        self.input = input
+        #self.input = Discretize.df.drop('CHANGE',axis='columns')    
         
     def df_to_csv(self, dataframe):
         dataframe.to_csv (r'discretized_dataframe.csv', index = False, header=True)
@@ -15,7 +16,8 @@ class Discretize:
     def pandas_dframe(self, np_discretized):
         discretized_dataframe = pd.DataFrame.from_records(np_discretized)
         discretized_dataframe.columns = self.input.columns
-        self.df_to_csv(discretized_dataframe)
+        #if df_to_csv:
+         #   self.df_to_csv(discretized_dataframe)
         return discretized_dataframe
 
         
