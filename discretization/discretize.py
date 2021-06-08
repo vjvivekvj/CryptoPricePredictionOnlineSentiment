a6f04856d22100cb2b859e5ad9293ee48b63b4c1
vjvivekvj <vivekj.joshi9@gmail.com>
2021-06-08 15:40:43 +0530

    driver code for discretize class

---
+++ discretization/discretize.py
@@ -0,0 +1,24 @@
+from discretization.discretization import Discretize
+import pandas as pd
+
+fname = Discretize.csv_input
+input_df = pd.read_csv(fname) 
+output_df =  pd.DataFrame(columns = input_df.columns)
+col_list = output_df.columns.tolist()
+
+def discretize(plist):              #plist is the list of tuples
+    global output_df
+    attr = 0
+    for tup in plist:
+        input_attr = input_df[col_list[attr]]
+        input_attr = input_attr.to_frame()
+        dobj = Discretize(input_attr)
+        nbins = int(tup[1], 2)
+        if nbins < 2:
+            nbins = 2
+        bin_opt = tup[0]
+        if bin_opt == 0:
+            temp_df = Discretize.equiwidth_binning(dobj,nbins)
+            output_df[col_list[attr]] = temp_df[col_list[attr]]
+        attr += 1
+    return output_df        
