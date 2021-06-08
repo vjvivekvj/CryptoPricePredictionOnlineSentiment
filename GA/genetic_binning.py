36eaeb962fc30fc21f9a5e3d7fb7987968ed7f27
vjvivekvj <vivekj.joshi9@gmail.com>
2021-06-09 01:00:38 +0530

    Update genetic_binning.py

--- GA/genetic_binning.py
+++ GA/genetic_binning.py
@@ -6,24 +6,29 @@ class GeneticBinning(Genetic):
     def __init__(self):
         super().__init__()
         
-    def fitness(self):
-        for genome in self.population:
-            plist = [] 
-            bin_opt = 0
-            nbins = ''
-            for i in range(len(genome)):
-                nbins += str(genome[i])
-                if i%4==0:
-                    bin_opt = genome[i]  
-                if i%4==3:
-                    plist.append((bin_opt,nbins))
-                    nbins = ''
-            df = discretize(plist)
-            fitness_score = tree_score(df)
-            print("".join(map(str,genome)), fitness_score)
-        return
+    def fitness(self,genome):
+        plist = [] 
+        bin_optn = 0
+        nbins = ''
+        for i in range(len(genome)):
+            nbins += str(genome[i])
+            if i%4==0:
+                bin_optn = genome[i]  
+            if i%4==3:
+                plist.append((bin_optn,nbins))
+                nbins = ''
                 
+        df = discretize(plist)
+        fitness_score = tree_score(df)
+
+        #store fitness scores in the dictionary
+        self.fitness_dict[fitness_score] = genome
+        print("".join(map(str,genome)), fitness_score)
+        return fitness_score
+                
+    def genome_to_text(genome):
+        pass
+
 
 gbin = GeneticBinning()
-gbin.create_population(2) #user input
-gbin.fitness()
+gbin.run_evolution(20, .90, 20)
